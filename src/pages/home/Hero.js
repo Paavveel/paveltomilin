463281a03da8110679cf96c0fb83ee65a5a7a662
Paavveel <pa.tomilin@gmail.com>
2022-04-13 12:06:09 +0300

    change name and dev position in Hero componet

--- src/pages/home/Hero.js
+++ src/pages/home/Hero.js
@@ -1,11 +1,13 @@
-import React, { useRef, useState } from 'react';
+import React, { useState } from 'react';
 import { motion, useAnimation } from 'framer-motion';
 import styled from 'styled-components';
 import { media } from '../../styles/GlobalStyles';
-import Social from '../../components/Social';
 import Tilt from 'react-parallax-tilt';
 import keyboard from '../../assets/keyboard.mp4';
 import { toggleNoScroll } from '../../utils/utils';
+
+// Conponents
+import Social from '../../components/Social';
 
 const StyledTilt = styled(Tilt)`
   height: 100vh;
@@ -14,33 +16,6 @@ const StyledTilt = styled(Tilt)`
 
   @media ${media.small} {
     transform: none !important;
-  }
-
-  .level--2 {
-    position: absolute;
-    left: 24%;
-    top: 29%;
-    width: 55%;
-    height: 52%;
-    z-index: 50;
-    opacity: 0.7;
-    transform: translateZ(-100px);
-  }
-  .level--1 {
-    position: absolute;
-    left: 27%;
-    top: 25%;
-    width: 55%;
-    height: 52%;
-    z-index: 70;
-    opacity: 0.9;
-    transform: translateZ(-50px);
-  }
-  .level-1 {
-  }
-  .level-2 {
-  }
-  .level-3 {
   }
 `;
 
@@ -60,10 +35,10 @@ const StyledName = styled(motion.h1)`
   line-height: 0.9;
   letter-spacing: 3px;
   text-transform: uppercase;
-  /* background: linear-gradient(85deg, #ba39f7, #4992f8, #64c0d3);
+  background: linear-gradient(85deg, #ba39f7, #4992f8, #64c0d3);
   background-clip: text;
   -webkit-background-clip: text;
-  -webkit-text-fill-color: transparent; */
+  -webkit-text-fill-color: transparent;
   transform: translateZ(100px);
 
   @media ${media.medium} {
@@ -81,26 +56,21 @@ const StyledName = styled(motion.h1)`
 
 const StyledSpan = styled(motion.span)`
   position: absolute;
-  left: -19%;
-  top: 55%;
+  bottom: -20%;
+  left: -10%;
   font-weight: 800;
   line-height: 0.9;
-  font-size: 3rem;
-  /* color: var(--blue); */
-  /* -webkit-text-fill-color: transparent;
-  -webkit-text-stroke: 0.1vw; */
+  font-size: 3.5rem;
   transform: translateZ(50px);
 
   @media ${media.medium} {
-    font-size: 2.5rem;
+    font-size: 3rem;
   }
   @media ${media.small} {
-    font-size: 2rem;
+    font-size: 2.5rem;
     left: unset;
-    top: unset;
     bottom: -18%;
     right: 0;
-    /* -webkit-text-stroke: 0.3vw; */
   }
   @media ${media.xsmall} {
     font-size: 2rem;
@@ -129,7 +99,7 @@ const squareVariants = {
   },
 };
 
-const titleVariants = {
+const nameVariants = {
   initial: { opacity: 0 },
   enter: {
     opacity: 1,
@@ -137,6 +107,21 @@ const titleVariants = {
       ease: 'easeInOut',
       duration: 0.5,
       delay: 2,
+    },
+  },
+  exit: {
+    opacity: 0,
+    transition: { ease: 'easeInOut', duration: 0.5 },
+  },
+};
+const spanVariants = {
+  initial: { opacity: 0 },
+  enter: {
+    opacity: 1,
+    transition: {
+      ease: 'easeInOut',
+      duration: 0.5,
+      delay: 2.4,
     },
   },
   exit: {
@@ -158,7 +143,6 @@ const videoVariats = {
 const Hero = () => {
   const [playTilt, setPlayTilt] = useState(false);
   const controls = useAnimation();
-  const tiltRef = useRef();
 
   return (
     <>
@@ -182,7 +166,7 @@ const Hero = () => {
           }}
         >
           <StyledName
-            variants={titleVariants}
+            variants={nameVariants}
             initial='initial'
             animate='enter'
             exit='exit'
@@ -191,7 +175,7 @@ const Hero = () => {
             Tomilin
           </StyledName>
           <StyledSpan
-            variants={titleVariants}
+            variants={spanVariants}
             initial='initial'
             animate='enter'
             exit='exit'
@@ -211,8 +195,6 @@ const Hero = () => {
             <source src={keyboard} type='video/mp4' />
           </StyledVideo>
         </StyledSquare>
-        {/* <StyledSquare className='level--1' />
-        <StyledSquare className='level--2' /> */}
       </StyledTilt>
 
       <Social />
